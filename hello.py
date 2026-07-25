e734aa599606c5b8fa6a482126b6b96b1f6b2e54
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-07-25 08:27:47 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -12,3 +12,21 @@ print(name, age)
 print ("I am learning python")
 
 print("It is awesome!")
+
+
+
+
+
+
+
+
+
+
+
+
+# Your flow is now:
+
+# ✅ git add hello.py
+# ✅ git commit
+# ⏳ git pull origin main
+# ✅ git push
