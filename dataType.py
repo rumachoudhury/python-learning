5512215688885fdbd9d15147b5b25787ba463703
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-07-26 08:46:55 -0400

    Add data type examples

--- dataType.py
+++ dataType.py
@@ -2,10 +2,23 @@
 # Setting the Data Type
 
 x = "Hello World"
-
 print(type(x))
 
 
 
 y = 20
 print(type(y))
+
+
+# create a list
+fruits = ["apple", "banana", "orange"]
+print(type(fruits))
+
+
+# tuple instead, use parentheses:
+fruits = ("apple", "banana", "orange")
+print(type(fruits))
+
+# =====================================
+# []----> creates a list.
+# ()-----> creates a tuple.
