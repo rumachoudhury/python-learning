212bbcf482ccec4b90a817552f411fdcf6ac4cfe
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-07-27 10:57:02 -0400

    Add number data type examples

---
+++ numbers.py
@@ -0,0 +1,25 @@
+
+# There are three numeric types in Python:
+
+# int
+# float
+# complex
+
+# =====================================
+
+
+# Example
+
+x = 10  # int
+
+y = 1.2  #float
+
+z = 1j # complex(no space before j)
+
+
+# To verify the type of any object in Python, use the type() function:
+print(type(x))
+
+print(type(y))
+
+print(type(z))
