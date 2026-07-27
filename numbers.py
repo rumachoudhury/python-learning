fe66dc990b0a543dca6e9f91306d64074def51ac
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-07-27 11:05:55 -0400

    Add integer, float and complex examples

--- numbers.py
+++ numbers.py
@@ -4,6 +4,7 @@
 # int
 # float
 # complex
+# In Python, you must use j (or J) to represent the imaginary part of a complex number.
 
 # =====================================
 
@@ -14,7 +15,7 @@ x = 10  # int
 
 y = 1.2  #float
 
-z = 1j # complex(no space before j)
+z = 1j # complex(no space before j) and must use j no other letter 
 
 
 # To verify the type of any object in Python, use the type() function:
@@ -22,4 +23,16 @@ print(type(x))
 
 print(type(y))
 
+print(type(z))
+
+
+
+# Integers:
+
+x = 1
+y = 35656222554887711
+z = -3255522
+
+print(type(x))
+print(type(y))
 print(type(z))
