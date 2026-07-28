77c8f78df11c679b45496da6eb9d9e6a591f85d8
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-07-28 11:01:51 -0400

    Add type conversion examples

--- numbers.py
+++ numbers.py
@@ -60,3 +60,19 @@ z = -5j
 print(x, type(x))
 print(y, type(y))
 print(z, type(z))
+
+# ============================================
+# Convert from one type to another:
+
+x = 1     #int
+y = 2.8   #float
+z = 1j    #complex
+
+#convert from int to float:
+a = float (x)
+
+#convert from float to int:
+b = int (y)
+
+#convert from int to complex
+c = complex (x)
