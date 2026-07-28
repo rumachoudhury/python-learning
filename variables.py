519b918a8604d89a33331ab4684c7c56fa51b201
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-07-28 17:16:42 -0400

    Add multi words variable names examples

--- variables.py
+++ variables.py
@@ -7,3 +7,29 @@
 x = "John"
 # is the same as
 x = 'John'
+
+
+# ===========================================
+# Variable names are case-sensitive.
+
+
+a = 4
+
+A = 4
+
+# A will not overwrite a
+
+
+# ================================================
+# Multi Words Variable Names
+
+# Camel Case
+myVariableName = "John"
+
+# Pascal Case
+MyVariableName = "John"
+
+
+# Snake Case
+# Each word is separated by an underscore character:
+my_variable_name = "John"
