0b82e752be4f1e21f3624fa83d10f0ab0275ae9e
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-07-29 12:22:04 -0400

    Add legal and illegal variable name examples

--- variables.py
+++ variables.py
@@ -33,3 +33,18 @@ MyVariableName = "John"
 # Snake Case
 # Each word is separated by an underscore character:
 my_variable_name = "John"
+
+# ================================================
+# Legal variable names:
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+# ================================================
+# Illegal variable names:
+2myvar = "John"
+my-var = "John"
+my var = "John"
