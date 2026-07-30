7708291df320ae151bf1245b0ff859446234b111
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-07-30 07:58:44 -0400

    Add multiline string example

--- strings.py
+++ strings.py
@@ -34,4 +34,14 @@ ut labore et dolore magna aliqua."""
 
 print(a)
 
+# -------------------------------------------------
+
+# Or three single quotes:
+a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+
+print(a)
+
 # ================================================
