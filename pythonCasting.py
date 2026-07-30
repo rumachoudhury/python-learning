6a9794c4bc1ba41ff36b0c86ca11a38f875dac83
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-07-29 23:40:24 -0400

    Add Python casting examples for int float and string

---
+++ pythonCasting.py
@@ -0,0 +1,27 @@
+
+# Casting in python is therefore done using constructor functions:
+# Casting means changing one data type into another.
+
+# int() → converts to integer (whole number) int(3.5)  # 3
+
+# float() → converts to decimal number float(5)  # 5.0
+
+# str() → converts to string (text) str(10)  # "10"
+
+
+# Integers:
+x = int(1) # x will be 1
+y = int(2.8) # y will be 2
+z = int("3") # z will be 3
+
+
+# Floats:
+x = float(1) #x will be 1.0
+y = float(2.8) #y will be 2.8
+z = float("3") #z will be 3.0
+w = float("4.2") #w will be 4.2
+
+# Strings:
+x = str("s1") #x will be 's1'
+y = str(2) #y will be '2'
+z = str(3.0) #z will be '3.0'
