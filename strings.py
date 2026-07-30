76acbfa5be1eaf2968d204009621cdcf1bcdc117
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-07-30 07:51:17 -0400

    Add quotes inside strings examples

--- strings.py
+++ strings.py
@@ -5,3 +5,9 @@
 # You can display a string literal with the print() function:
 print("Hello")
 print('Hello')
+
+
+# Quotes Inside Quotes
+# You can use quotes inside a string
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
