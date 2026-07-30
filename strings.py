b92682c7005e3d5450f97eacfdc1364e57dbe3e1
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-07-30 07:56:57 -0400

    Add multiline string example

--- strings.py
+++ strings.py
@@ -21,3 +21,17 @@ print('He is called "Johnny"')
 
 a = "Hello"
 print(a)
+
+
+# ================================================
+
+# You can assign a multiline string to a variable by using three quotes:
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+
+print(a)
+
+# ================================================
