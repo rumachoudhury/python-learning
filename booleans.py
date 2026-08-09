a90a4d1e57cd4d05c7f6053f343427c451e599fd
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-08-09 10:03:08 -0400

    Add bool values and variables examples

--- booleans.py
+++ booleans.py
@@ -8,6 +8,7 @@ print(10 < 9)
 
 
 # ========================================
+# Print a message based on whether the condition is True or False:
 
 a = 200
 b = 33
@@ -16,3 +17,20 @@ if b > a:
     print("b is greater then a")
 else:
     print("b is not greater then a")
+
+
+# ============================================
+# Evaluate Values and Variables
+
+# Evaluate a string and a number:
+print(bool("hello"))
+print(bool(15))
+
+# ============================================
+# Evaluate two variables:
+
+x = "Hello"
+y = 15
+
+print(bool(x))
+print(bool(y))
