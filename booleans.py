77dbf25b40ec470b6a7bc8da3a519bdd2435f565
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-08-09 09:58:00 -0400

    Add if else boolean example

--- booleans.py
+++ booleans.py
@@ -4,3 +4,15 @@ print(10 > 9)
 print(10 == 9)
 
 print(10 < 9)
+
+
+
+# ========================================
+
+a = 200
+b = 33
+
+if b > a:
+    print("b is greater then a")
+else:
+    print("b is not greater then a")
