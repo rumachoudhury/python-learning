335d3100c47fdd9b093d8a261a63f2c602acb519
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-08-10 13:11:51 -0400

    Add variable calculation examples

---
+++ operator.py
@@ -0,0 +1,10 @@
+
+# Operators are used to perform operations on variables and values.
+
+print(10 + 5) # we use the + operator to add together two values:
+
+sum1 = 100 + 50 
+sum2 = sum1 + 200
+sum3 = sum2 + 300
+
+print(sum1, sum2, sum3)
