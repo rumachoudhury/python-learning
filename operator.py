00aa399c9b314dcda74f201084be826473921ecb
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-08-10 13:15:54 -0400

    Add arithmetic operators examples

--- operator.py
+++ operator.py
@@ -8,3 +8,17 @@ sum2 = sum1 + 200
 sum3 = sum2 + 300
 
 print(sum1, sum2, sum3)
+
+# ============================================
+# Here is an example using different arithmetic operators:
+
+x = 15
+y = 4
+
+print(x + y)   # Addition
+print(x - y)   # Subtraction
+print(x * y)   # Multiplication
+print(x / y)   # Division
+print(x % y)   # Modulus
+print(x ** y)  # Exponentiation
+print(x // y)  # Floor division
