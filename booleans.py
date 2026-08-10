f39c4fddd50c50a3ea1130da2a5e9d364b852900
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-08-10 13:02:15 -0400

    Add isinstance integer example

--- booleans.py
+++ booleans.py
@@ -59,3 +59,11 @@ if myFunction():
    print("YES")
 else:
     print("No")
+
+
+# ============================================
+# Python also has many built-in functions that return a boolean value, like the isinstance() function, which can be used to determine if an object is of a certain data type:
+
+# Check if an object is an integer or not:
+x = 200
+print(isinstance(x, int))
