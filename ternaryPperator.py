a76d900977d596f5663bc034060d82083b3cc109
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-08-13 23:03:55 -0400

    Add conditional expression example

---
+++ ternaryPperator.py
@@ -0,0 +1,12 @@
+
+# The ternary operator allows you to assign one value if a condition is true, and another if it is false:
+
+# Assign a value to x:
+
+# Assign the value "WEEKEND!" if the number is higher than 5, otherwise "Workday":
+
+num = 6
+
+x = "WEEKEND!" if num > 5 else "Workday"
+
+print(x)
