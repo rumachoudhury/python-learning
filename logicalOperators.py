096bfa1a38454c46201bf0064333edac8a2a563b
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-08-14 23:10:01 -0400

    Add and or logical operators examples

---
+++ logicalOperators.py
@@ -0,0 +1,23 @@
+
+# Logical operators are used to combine conditional statements: 
+# and  --->Returns True if both statements are true	x < 5 and  x < 10
+# or   --->Returns True if one of the statements is true	x < 5 or x < 4
+# not  --->Reverse the result, returns False if the result is true	not(x < 5 and x < 10)
+
+
+# ============================================
+
+# Test if a number is greater than 0 and less than 10:
+
+x = 5
+
+print(x > 0 and x < 10)
+
+
+# ============================================
+
+# Test if a number is less than 5 or greater than 10:
+
+x = 5
+
+print(x < 5 or x > 10)
