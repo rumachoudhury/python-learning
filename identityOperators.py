139b197e983aad44571e589aced5200e073bcb07
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-08-14 23:48:01 -0400

    Add identity operators example

---
+++ identityOperators.py
@@ -0,0 +1,13 @@
+# Identity Operators
+
+# Identity operators are used to compare the objects, not if they are equal, but if they are actually the same object, with the same memory location:
+
+# The is operator returns True if both variables point to the same object:
+
+x = ["apple", "banana"]
+y = ["apple", "banana"]
+z = x
+
+print(x is z)
+print(x is y)
+print(x == y)
