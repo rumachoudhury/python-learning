566bceabef8b225546f8792b164f54635e547ebc
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-08-14 23:45:30 -0400

    Add not logical operator example

--- logicalOperators.py
+++ logicalOperators.py
@@ -21,3 +21,11 @@ print(x > 0 and x < 10)
 x = 5
 
 print(x < 5 or x > 10)
+
+# ============================================
+
+# Reverse the result with not:
+
+x = 5
+
+print(not(x > 3 and x < 10))
