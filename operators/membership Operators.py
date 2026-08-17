c4934bbe0842220ffea8730de58fbb6fa866857d
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-08-17 09:25:29 -0400

    Add membership operator example

---
+++ operators/membership Operators.py
@@ -0,0 +1,11 @@
+
+
+# Membership operators are used to test if a sequence is presented in an object:
+
+Description: 
+# in--->	Returns True if a sequence with the specified value is present in the object
+
+# not in--->	Returns True if a sequence with the specified value is not present in the object
+
+fruits = ["apple", "banana", "cherry"]
+print("orange" not in fruits)
