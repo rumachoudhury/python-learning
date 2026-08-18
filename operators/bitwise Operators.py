5525ccd6ecfb45fbb86a8087b49b7a85173d460c
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-08-18 09:38:47 -0400

    Update bitwise operators notes

--- operators/bitwise Operators.py
+++ operators/bitwise Operators.py
@@ -26,6 +26,7 @@ The binary representation of 3 is 0011
 
 
 # ============================================
+
 # Easy way to remember: & gives 1 only when both bits are 1.
 
 1 & 1 = 1
@@ -39,4 +40,23 @@ The binary representation of 3 is 0011
 #   0010   → 2
 
 # This is called the bitwise AND operator.
+
+# ============================================
+
+# The | operator compares each bit and set it to 1 if one or both is 1, otherwise it is set to 0:
+
+print(6 | 3)
+The binary representation of 6 is 0110
+The binary representation of 3 is 0011
+
+
+# The ^ operator sets each bit to 1 if the two bits are different. If they are the same, it sets the bit to 0.
+#   0110   → 6
+# ^ 0011   → 3
+# ------
+#   0101   → 5
+
+
+# Then the | operator compares the bits and returns 0111, which is 7 in decimal.
+
 # ============================================
