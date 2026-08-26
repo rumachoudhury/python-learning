0e8d9c680a21e43d3845bf6dcff96c1b9cd1a979
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-08-26 00:23:47 -0400

    Add extend list example

--- Lists/addListItems.py
+++ Lists/addListItems.py
@@ -26,3 +26,16 @@ print(addlist)
 thislist = ["apple", "banana", "cherry"]
 thislist.insert(1, "orange")
 print(thislist)
+
+
+# ===========================================
+
+# Extend List
+# To append elements from another list to the current list, use the extend() method.
+
+# Add the elements of tropical to thislist:
+
+thislist = ["apple", "banana", "cherry"]
+tropical = ["mango", "pineapple", "papaya"]
+thislist.extend(tropical)
+print(thislist)
