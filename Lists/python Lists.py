d1f296f5937409ffea9ae4ba25aa5806a8a870a4
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-08-25 23:41:56 -0400

    Add list length example

--- Lists/python Lists.py
+++ Lists/python Lists.py
@@ -9,3 +9,14 @@ print(mylist)
 
 thislist = ["apple", "banana", "cherry", "apple", "cherry"]
 print(thislist)
+
+# ============================================
+
+# List Length
+# To determine how many items a list has, use the len() function:
+
+
+# Print the number of items in the list:
+
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
