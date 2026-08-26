d9534734bf6f7c72a602fffdf4ca8bac4bed5d84
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-08-26 00:16:50 -0400

    Add append list item example

---
+++ Lists/addListItems.py
@@ -0,0 +1,14 @@
+
+
+# Python - Add List Items
+# Append Items
+
+addlist = ["apple", "banana", "cherry"]
+
+addlist.append("orange")
+
+print(addlist)
+
+# Output:
+
+# ['apple', 'banana', 'cherry', 'orange']
