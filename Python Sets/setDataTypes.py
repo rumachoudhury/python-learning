09e433bd253d48a4a3c9964d388b337d13f7637d
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-09-16 18:46:18 -0400

    Add different Python set examples

---
+++ Python Sets/setDataTypes.py
@@ -0,0 +1,14 @@
+
+# Set items can be of any data type:
+
+set1 = {"apple", "banana", "cherry"}
+
+set2 = {1, 5, 7, 9, 3}
+
+set3 = {True, False, False}
+
+print(set1)
+print(set2)
+print(set3)
+
+# set3 will only contain True and False because duplicate False is automatically removed.
