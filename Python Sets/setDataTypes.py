f040ddae9171b67036168c7d30b2129d3c96a254
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-09-16 18:50:11 -0400

    Add Python set data types and constructor examples

--- Python Sets/setDataTypes.py
+++ Python Sets/setDataTypes.py
@@ -12,3 +12,23 @@ print(set2)
 print(set3)
 
 # set3 will only contain True and False because duplicate False is automatically removed.
+
+# ================================
+
+# A set can contain different data types:
+
+
+# A set with strings, integers and boolean values:
+
+set1 = {"abc", 34, True, 40, "male"}
+print(set1)
+
+# =================================
+# The set() Constructor
+# It is also possible to use the set() constructor to make a set.
+
+
+# Using the set() constructor to make a set:
+
+thisset = set(("apple", "banana", "cherry")) # note the double round-brackets
+print(thisset)
