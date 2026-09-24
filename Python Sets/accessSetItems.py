31873c4704283d51be3a9106d9f944be78c64b00
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-09-23 23:31:54 -0400

    Add Python set example

---
+++ Python Sets/accessSetItems.py
@@ -0,0 +1,15 @@
+
+# You cannot access items in a set by referring to an index or a key.
+
+# But you can loop through the set items using a for loop, or ask if a specified value is present in a set, by using the in keyword.
+
+my_set = {"apple", "banana", "cherry"}
+
+# Loop through the set items
+for item in my_set:
+    print(item)
+
+# Check if a specified value is present in the set
+print("apple" in my_set)
+print("orange" in my_set)
+
