1adc6c87aea037dfea7fe37441165750a07d04df
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-09-24 23:58:31 -0400

    Add symmetric difference methods

--- Python Sets/joinSets.py
+++ Python Sets/joinSets.py
@@ -107,3 +107,29 @@ set2 = {"google", "microsoft", "apple"}
 set1.difference_update(set2)
 
 print(set1)
+
+# ==============================
+# The symmetric_difference() method will keep only the elements that are NOT present in both sets.
+
+# Example
+# Keep the items that are not present in both sets:
+
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+
+set3 = set1.symmetric_difference(set2)
+
+print(set3)
+
+
+# ==============================
+# You can use the ^ operator instead of the symmetric_difference() method, and you will get the same result.
+
+
+# Use ^ to join two sets:
+
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+
+set3 = set1 ^ set2
+print(set3)
