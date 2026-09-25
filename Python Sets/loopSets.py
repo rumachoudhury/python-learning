b9f123301ba3dcf42e357519bd65bc92c2a9605b
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-09-24 23:38:19 -0400

    Add looping through set items

---
+++ Python Sets/loopSets.py
@@ -0,0 +1,14 @@
+
+
+# You can loop through the set items by using a for loop:
+
+thisset = {"apple", "banana", "cherry"}
+
+for x in thisset:
+  print(x)
+
+
+myset = {"orange", "grape", "melon"}
+
+for x in myset:
+  print(x)
