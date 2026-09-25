4d3fa69738a655d990162007a10a1f60bb9236b9
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-09-24 23:56:31 -0400

    Add difference_update method

--- Python Sets/joinSets.py
+++ Python Sets/joinSets.py
@@ -93,3 +93,17 @@ print(set3)
 # Note: The & operator only allows you to join sets with sets, and not with other data types like you can with the intersection() method.
 
 # The intersection_update() method will also keep ONLY the duplicates, but it will change the original set instead of returning a new set.
+
+
+# ==============================
+
+# The difference_update() method will keep the items from the first set that are not in the other set, but it will change the original set instead of returning a new set.
+
+# Use the difference_update() method to keep only the items from the first set that are not present in the other set:
+
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+
+set1.difference_update(set2)
+
+print(set1)
